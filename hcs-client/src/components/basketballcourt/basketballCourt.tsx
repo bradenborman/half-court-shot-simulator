e3f6ea42d0fe3010ec55ee0dc50ab282af82ddd1
Braden Borman <bborman@shelterinsurance.com>
2020-10-17 21:25:30 -0500

    Will cycle through with delay now yee yee

--- hcs-client/src/components/basketballcourt/basketballCourt.tsx
+++ hcs-client/src/components/basketballcourt/basketballCourt.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { SimulationResult } from "../../models/simulationResult";
+import { AttemptBreakdown } from "../../models/attemptBreakdown";
 
 require("./basketballCourt.scss");
 
@@ -10,6 +11,42 @@ export interface IBasketballCourtProps {
 export const BasketballCourt: React.FC<IBasketballCourtProps> = (
   props: IBasketballCourtProps
 ) => {
+  const [currentLoction, setCurrentLocation] = useState<String | null>(
+    props.simulationResult.startingLocation.replace("_", " ")
+  );
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [cycleCompleted, setCycleCompleted] = useState<boolean>(false);
+
+  useEffect(() => {
+    cycleThrough();
+  }, [cycleCompleted]);
+
+  const cycleThrough = async () => {
+    await timeout(500); //for half sec delay
+    const attemptBreakdown: AttemptBreakdown =
+      props.simulationResult.allAttempts[currentIndex];
+    const shots: String[] = attemptBreakdown.shotsAttemptedCurrentTry.map(
+      shot => {
+        return shot.replace("_", " ");
+      }
+    );
+
+    const completed = updateShot(shots);
+    setCurrentIndex(currentIndex + 1);
+    setCycleCompleted(!cycleCompleted);
+  };
+
+  function timeout(delay: number) {
+    return new Promise(res => setTimeout(res, delay));
+  }
+
+  const updateShot = async (shots: String[]) => {
+    shots.forEach(shot => {
+      setCurrentLocation(shot);
+    });
+    return true;
+  };
+
   return (
     <div className="basketball-court">
       <div className="court-img"></div>
@@ -19,6 +56,7 @@ export const BasketballCourt: React.FC<IBasketballCourtProps> = (
           Starting from:{" "}
           {props.simulationResult.startingLocation.replace("_", " ")}
         </p>
+        <p>Attempting shot from: {currentLoction}</p>
       </div>
     </div>
   );
