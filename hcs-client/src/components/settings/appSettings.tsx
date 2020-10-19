0e2debc5b23a1b737de41167b6c42591bb286669
Braden Borman <bborman@shelterinsurance.com>
2020-10-18 21:12:35 -0500

    Added description to ui of purpose and info for user

--- hcs-client/src/components/settings/appSettings.tsx
+++ hcs-client/src/components/settings/appSettings.tsx
@@ -13,7 +13,7 @@ export const AppSettings: React.FC<IAppSettingsProps> = (
 ) => {
   return (
     <div className="settings-wrapper">
-      <h2>Settings:</h2>
+      <h2 id="settings-title">Settings:</h2>
       <div className="speed-control">
         <p>Current pace: {props.currentSpeed / 1000} seconds</p>
         <div onClick={() => props.updateSpeed(0.5)} className="speed-btn">
@@ -21,6 +21,31 @@ export const AppSettings: React.FC<IAppSettingsProps> = (
         </div>
         <div onClick={() => props.updateSpeed(-0.5)} className="speed-btn">
           Faster
+        </div>
+      </div>
+      <div className="app-info-wrapper">
+        <h2 id="app-title">"Half Court Simulator"</h2>
+        <div className="app-info">
+          <p>
+            <b>About: </b>
+            Two simultaneous simulations race against each other to be the first
+            one to complete all four shots in succession. They will start on
+            opposite ends of the challenge. (Half Court and Layup).
+          </p>
+          <p>
+            <b>Goal: </b>
+            With this simulation, viewers can see which process has the higher
+            success rate.
+          </p>
+          <p>
+            <b>Shot Success Percents: </b>
+            <ul>
+              <li>Half Court {"=>"} 8.5%</li>
+              <li>Three Pointer {"=>"} 27.5%</li>
+              <li>Free Thorw {"=>"} 66%</li>
+              <li>Layup {"=>"} 96%</li>
+            </ul>
+          </p>
         </div>
       </div>
     </div>
